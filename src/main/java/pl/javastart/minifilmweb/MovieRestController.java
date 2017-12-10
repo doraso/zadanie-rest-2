f9cbd4639fa6624656c5a6305391dddd0fda3115
Dorota Szwed <dorota.soroko@gmail.com>
2017-12-10 23:50:25 +0100

    dopisanie RestControllera

--- src/main/java/pl/javastart/minifilmweb/MovieRestController.java
+++ src/main/java/pl/javastart/minifilmweb/MovieRestController.java
@@ -1,8 +1,8 @@
 package pl.javastart.minifilmweb;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +16,47 @@ public class MovieRestController {
     public List<Movie> getAllMovies() {
         System.out.println("Odpytanie o wszystkie filmy");
         return movieRepository.findAll();
+    }
+
+
+    @GetMapping("/api/movies/{id}")
+    public ResponseEntity getMovie(@PathVariable Long id) {
+        Movie movie = movieRepository.findOne(id);
+        if (movie != null) {
+            return ResponseEntity.ok(movie);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/api/movies")
+    public ResponseEntity addMovie(@RequestBody Movie movie) {
+        Movie savedMovie = movieRepository.save(movie);
+        return ResponseEntity.ok(savedMovie);
+    }
+
+    @PutMapping("/api/movies/{id}")
+    public ResponseEntity changeMovie(@PathVariable long id, @RequestBody Movie movie) {
+        Movie existMovie = movieRepository.findOne(id);
+        if (existMovie != null) {
+            movie.setId(existMovie.getId());
+            movie.setTitle(existMovie.getTitle());
+            movie.setPremiereDate(existMovie.getPremiereDate());
+            movieRepository.save(movie);
+
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/api/movies/{id}")
+    public ResponseEntity deleteMovie(@PathVariable long id) {
+        Movie movie = movieRepository.findOne(id);
+        if (movie != null) {
+            movieRepository.delete(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
